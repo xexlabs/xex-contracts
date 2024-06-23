37982c4362d8378213a34c66b04f6f50285991e4
regen <120606503+xexcrypto@users.noreply.github.com>
2024-06-23 10:45:56 -0300

    Added the GameMain.sol contract.

---
+++ contracts/Game/GameMain.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.26;
+
+import {GameCore} from "./GameCore.sol";
+
+contract GameMain is GameCore {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant DUGEON_ROLE = keccak256("DUGEON_ROLE");
+    constructor(address _xex_) GameCore(_xex_) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(DUGEON_ROLE, msg.sender);
+    }
+    function setAdmin(address account, bool isAdmin) external onlyRole(ADMIN_ROLE) {
+        if (isAdmin) {
+            grantRole(ADMIN_ROLE, account);
+        } else {
+            revokeRole(ADMIN_ROLE, account);
+        }
+    }
+
+    function setMinter(address account, bool isMinter) external onlyRole(ADMIN_ROLE) {
+        if (isMinter) {
+            grantRole(MINTER_ROLE, account);
+        } else {
+            revokeRole(MINTER_ROLE, account);
+        }
+    }
+
+    function addDungeon(
+        string memory _name,
+        uint _startIn,
+        uint _endIn,
+        uint _minTermDate,
+        uint _maxTermDate,
+        uint _minMintFee,
+        uint _difficulty,
+        uint _availableRewards
+    ) external onlyRole(DUGEON_ROLE) {
+        uint dungeonId = _dungeons.length() + 1;
+        _dungeons.add(dungeonId);
+        _dungeonInfo[dungeonId] = Dungeon(_name, _startIn, _endIn, _minTermDate, _maxTermDate, _minMintFee, _difficulty, true, _availableRewards, 0);
+        rewardsPool += _availableRewards;
+        emit DungeonAdded(dungeonId, _name, _startIn, _endIn, _minTermDate, _maxTermDate, _minMintFee, _difficulty, _availableRewards);
+    }
+
+    function updateDungeon(
+        uint _dungeonId,
+        string memory _name,
+        uint _startIn,
+        uint _endIn,
+        uint _minTermDate,
+        uint _maxTermDate,
+        uint _minMintFee,
+        uint _difficulty,
+        bool _active
+    ) external onlyRole(DUGEON_ROLE) {
+        if (!_dungeons.contains(_dungeonId)) revert DungeonNotFound();
+        Dungeon storage dungeon = _dungeonInfo[_dungeonId];
+        dungeon.name = _name;
+        dungeon.startIn = _startIn;
+        dungeon.endIn = _endIn;
+        dungeon.minTermDate = _minTermDate;
+        dungeon.maxTermDate = _maxTermDate;
+        dungeon.minMintFee = _minMintFee;
+        dungeon.difficulty = _difficulty;
+        dungeon.active = _active;
+        emit DungeonUpdated(_dungeonId, _name, _startIn, _endIn, _minTermDate, _maxTermDate, _minMintFee, _difficulty, _active);
+    }
+    function setSigner(address _newSigner) external onlyRole(ADMIN_ROLE) {
+        if (_newSigner == address(0)) revert InvalidSigner();
+        _signer = _newSigner;
+        emit NewSigner(_newSigner);
+    }
+    function setBaseURI(string memory _baseURI_) external onlyRole(ADMIN_ROLE) {
+        if (baseURI_ == _baseURI_) revert InvalidBaseURI();
+        if (bytes(_baseURI_).length == 0) revert InvalidBaseURI();
+        baseURI_ = _baseURI_;
+        emit NewBaseURI(_baseURI_);
+    }
+}
--- test/Game.ts
+++ test/Game.ts
@@ -2,19 +2,88 @@ Error.stackTraceLimit = Infinity
 import { time, loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers'
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
+import { Game, GameNFT, XEX } from '../typechain-types/contracts'
+import { Signer } from 'ethers'
 const toWei = ethers.parseEther
 describe('Game', function () {
 	const depositAmount = toWei('100')
+	const timestamp = async () => BigInt(await timestamp())
 	async function deploy() {
 		const [owner, gamer1, gamer2] = await ethers.getSigners()
 		const XEX = await ethers.getContractFactory('XEX')
-		const Game = await ethers.getContractFactory('GameNFT')
-		const xex = await XEX.deploy()
-		const game = await Game.deploy()
+		const Game = await ethers.getContractFactory('GameMain')
+		const GameNFT = await ethers.getContractFactory('GameNFT')
+		const nft = (await GameNFT.deploy()) as GameNFT
+		const xex = (await XEX.deploy()) as XEX
+		const game = (await Game.deploy(xex.target)) as Game
 		await xex.addMinter(game.target)
-		return { game, owner, gamer1, gamer2, xex }
+		return { game, owner, gamer1, gamer2, xex, nft }
 	}
+	async function addDungeon(
+		game: Game, // the game contract to use
+		owner: Signer, // the owner of the game contract
+		name: string, // the name of the dungeon
+		startIn: bigint, // the start date of the dungeon
+		endIn: bigint, // the end date of the dungeon
+		minTermDate: bigint, // the minimum term date of the dungeon
+		maxTermDate: bigint, // the maximum term date of the dungeon
+		minMintFee: bigint, // the minimum mint fee of the dungeon
+		difficulty: bigint, // the difficulty of the dungeon
+		availableRewards: bigint // the available rewards of the dungeon
+	) {
+		await expect(game.connect(owner).addDungeon(name, startIn, endIn, minTermDate, maxTermDate, minMintFee, difficulty, availableRewards))
+			.to.emit(game, 'DungeonAdded')
+			.withArgs(1, name, startIn, endIn, minTermDate, maxTermDate, minMintFee, difficulty, availableRewards)
 
+		return await game.getDungeonInfo(1)
+	}
+	describe('addDungeon', function () {
+		it('should add a new dungeon correctly', async function () {
+			const { game, owner } = await loadFixture(deploy)
+			const startIn = (await timestamp()) + 3600n // 1 hour from now
+			const endIn = startIn + 86400n // 1 day after start
+			const minTermDate = 36000n // 1 hour
+			const maxTermDate = 86400n // 1 day
+			const minMintFee = toWei('0.1') // 0.1 ETH
+			const difficulty = 50n // Medium difficulty
+			const name = 'Test Dungeon'
+			const availableRewards = toWei('1000') // 1000 XEX tokens
+			const dungeon = await addDungeon(game, owner, name, startIn, endIn, minTermDate, maxTermDate, minMintFee, difficulty, availableRewards)
+			expect(dungeon.name).to.equal(name)
+			expect(dungeon.startIn).to.equal(startIn)
+			expect(dungeon.endIn).to.equal(endIn)
+			expect(dungeon.minTermDate).to.equal(minTermDate)
+			expect(dungeon.maxTermDate).to.equal(maxTermDate)
+			expect(dungeon.minMintFee).to.equal(minMintFee)
+			expect(dungeon.difficulty).to.equal(difficulty)
+			expect(dungeon.active).to.be.true
+			expect(dungeon.availableRewards).to.equal(availableRewards)
+			expect(dungeon.claimedRewards).to.equal(0)
+
+			expect(await game.rewardsPool()).to.equal(availableRewards)
+		})
+
+		it('should only allow owner to add a dungeon', async function () {
+			const { game, gamer1 } = await loadFixture(deploy)
+			const startIn = (await timestamp()) + 3600n
+			const endIn = startIn + 86400n
+			await expect(game.connect(gamer1).addDungeon('Test Dungeon', startIn, endIn, 3600n, 86400n, toWei('0.1'), 50n, toWei('1000'))).to.be.revertedWith(
+				'Ownable: caller is not the owner'
+			)
+		})
+
+		it('should increment dungeon ID correctly', async function () {
+			const { game, owner } = await loadFixture(deploy)
+			const startIn = (await timestamp()) + 3600
+			const endIn = startIn + 86400
+
+			await game.connect(owner).addDungeon('Dungeon 1', startIn, endIn, 3600, 86400, toWei('0.1'), 50, toWei('1000'))
+			await game.connect(owner).addDungeon('Dungeon 2', startIn, endIn, 3600, 86400, toWei('0.1'), 50, toWei('1000'))
+
+			expect(await game.getDungeonInfo(1)).to.have.property('name', 'Dungeon 1')
+			expect(await game.getDungeonInfo(2)).to.have.property('name', 'Dungeon 2')
+		})
+	})
 	describe('Dungeon Management', function () {
 		it('should add a new dungeon', async function () {
 			const { game, owner, xex } = await loadFixture(deploy)
@@ -108,7 +177,7 @@ describe('Game', function () {
 			const rewardAmount = depositAmount
 
 			await game.addDungeon(name, startIn, endIn, minMintFee, minTermDate, maxTermDate, failurePercentage, xex.target, rewardAmount)
-			
+
 			// advance time to start the game
 			await time.increaseTo(startIn + 5)
 			await game.connect(gamer1).start(0, { value: minMintFee })
@@ -127,7 +196,7 @@ describe('Game', function () {
 			const rewardAmount = depositAmount
 
 			await game.addDungeon(name, startIn, endIn, minMintFee, minTermDate, maxTermDate, failurePercentage, xex.target, rewardAmount)
-			
+
 			// advance time to start the game
 			await time.increaseTo(startIn + 5)
 			await game.connect(gamer1).start(0, { value: minMintFee })
